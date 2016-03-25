d8b1d67234370196c0eaf1b15344c0b3158269aa
Brittany Rausch <berausch247@gmail.com>
2016-03-25 13:57:48 -0700

    Add some jquery and some branching that will help select the right city for the user. Also, the destinations were added in the html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,58 @@
+$(function() {
+  $(".sect1-btn").click(function(event){
+    event.preventDefault();
+    var q1 = $("input:radio[name=q1]:checked").val();
+    var q2 = $("input:radio[name=q2]:checked").val();
+    if(q2 === "nature") {
+      $("#nature").show();
+      $("#sect1").hide();
+    } else if (q2 === "city") {
+      $("#city").show();
+      $("#sect1").hide();
+    }
+  });
+  $(".sect2-btn").click(function(event){
+    event.preventDefault();
+    var q3Nature = $("input:radio[name=q3-nature]:checked").val();
+    alert(q3Nature);
+    if (q3Nature === "beach") {
+      $(".hawaii").show();
+      $("#nature").hide();
+    } else if (q3Nature == "woods"){
+      $(".yosemite").show();
+      $("#nature").hide();
+    }
+  });
+  $(".sect3-btn").click(function(event){
+    event.preventDefault();
+    var q1 = $("input:radio[name=q1]:checked").val();
+    var q3City = $("input:radio[name=q3-city]:checked").val();
+    var q4City = $("input:radio[name=q4-city]:checked").val();
+    var q5City = $("input:radio[name=q5-city]:checked").val();
+    $("#error").hide();
+    $(".new-york").hide();
+    $(".paris").hide();
 
+    if (q3City === q4City) {
+      $("#error").show();
+    } else if (q1 === "relaxing") {
+        if(q3City === "museums"){
+          if(q4City === "roller") {
+            if(q5City === "stay") {
+              $(".new-york").show();
+            } else if (q5City === "abroad"){
+              $(".paris").show();
+            }
+
+          }
+
+        }
+    }
+    //   $(".hawaii").show();
+    //   $("city").hide();
+    // } else if (q3Nature == "woods") {
+    //   $(".yosemite").show();
+    //   $("#nature").hide();
+    // }
+  });
+});
