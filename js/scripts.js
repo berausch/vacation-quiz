8e1e18a5c3f9d22f92b77debe6879284862ae8f4
Brittany Rausch <berausch247@gmail.com>
2016-03-25 15:06:53 -0700

    Make the website functional from a js standpoint. All the js and jquery was added and now the quiz works

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,16 @@ $(function() {
       $("#nature").show();
       $("#sect1").hide();
     } else if (q2 === "city") {
-      $("#city").show();
+      if (q1 === "cultural"){
+        $("#q5City").show();
+        $(".sect3-btn").show();
+        $("#sect1").hide();
+      } else {
+      $("#q3City").show();
+      $("#q4City").show();
+      $(".sect3-btn").show();
       $("#sect1").hide();
-    }
+      }}
   });
   $(".sect2-btn").click(function(event){
     event.preventDefault();
@@ -32,27 +39,143 @@ $(function() {
     $("#error").hide();
     $(".new-york").hide();
     $(".paris").hide();
-
+    $(".vegas").hide();
+    $(".disneyworld").hide();
+    $("#q5City").hide();
+    
     if (q3City === q4City) {
       $("#error").show();
-    } else if (q1 === "relaxing") {
+    }
+      else if (q1 === "relaxing") {
         if(q3City === "museums"){
           if(q4City === "roller") {
+            $("#q5City").show();
             if(q5City === "stay") {
               $(".new-york").show();
-            } else if (q5City === "abroad"){
+            }
+            else if (q5City === "abroad"){
+                $(".paris").show();
+            }}
+          else if (q4City === "theater") {
               $(".paris").show();
             }
-
-          }
-
+          else if (q4City === "gambling") {
+            $("#q5City").show();
+            if(q5City === "stay") {
+              $(".new-york").show();
+            }
+            else if (q5City === "abroad"){
+              $(".paris").show();
+            }}}
+        else if (q3City === "roller"){
+          $(".disneyworld").show();
         }
-    }
-    //   $(".hawaii").show();
-    //   $("city").hide();
-    // } else if (q3Nature == "woods") {
-    //   $(".yosemite").show();
-    //   $("#nature").hide();
-    // }
+        else if (q3City === "theater"){
+          $(".new-york").show();
+        }
+        else if (q3City === "gambling"){
+          $(".vegas").show();
+        }}
+    else if (q1 === "romantic"){
+      if(q3City === "museums") {
+        $(".paris").show();
+      }
+      else if (q3City === "roller"){
+        $(".disneyworld").show();
+      }
+      else if(q3City === "theater") {
+        if (q4City === "museums") {
+          $(".disneyworld").show();
+        } else if (q4City === "roller" ||q4City === "gambling") {
+          $(".new-york").show();
+        }}
+      else if (q3City === "gambling"){
+        $(".vegas").show();
+        }}
+    else if (q1 === "exciting"){
+      if (q3City === "museums") {
+        if(q4City === "roller" || q4City === "gambling") {
+          $("#q5City").show();
+          if(q5City === "stay") {
+            $(".new-york").show();
+          }
+          else if (q5City === "abroad"){
+              $(".paris").show();
+          }}
+        else if (q4City === "theater") {
+          $(".paris").show();
+        }}
+      else if (q3City === "roller") {
+        if (q4City === "museums"){
+          $(".vegas").show();
+        }
+        else if (q4City === "theater" || q4City === "gambling"){
+          $(".disneyworld").show();
+        }}
+      else if (q3City === "theater") {
+        $(".new-york").show();
+      }
+      else if (q3City === "gambling") {
+        $(".vegas").show();
+      }}
+    else if (q1 === "adventurous") {
+      if (q3City === "museums") {
+        if(q4City === "roller" || q4City === "gambling") {
+          $("#q5City").show();
+          if(q5City === "stay") {
+            $(".new-york").show();
+          }
+          else if (q5City === "abroad"){
+              $(".paris").show();
+          }}
+        else if (q4City === "theater") {
+          $(".paris").show();
+        }}
+      else if (q3City === "roller") {
+        $(".disneyworld").show();
+      }
+      else if (q3City === "theater"){
+        if(q4City === "museums" || q4City === "gambling") {
+          $(".disneyworld").show();
+        }
+        else if (q4City === "roller"){
+          $(".new-york").show();
+        }}
+      else if (q3City === "gambling") {
+        $(".vegas").show();
+      }}
+    else if (q1 === "family"){
+      if (q3City === "museums") {
+        if(q4City === "roller" || q4City === "gambling") {
+          $("#q5City").show();
+          if(q5City === "stay") {
+            $(".new-york").show();
+          }
+          else if (q5City === "abroad"){
+              $(".paris").show();
+          }}
+        else if (q4City === "theater") {
+          $(".paris").show();
+        }}
+        else if (q3City === "roller") {
+          $(".disneyworld").show();
+        }
+        else if (q3City === "theater"){
+          if(q4City === "museums" || q4City === "gambling") {
+            $(".disneyworld").show();
+          }
+          else if (q4City === "roller"){
+            $(".new-york").show();
+          }}
+        else if (q3City === "gambling") {
+          $(".vegas").show();
+      }}
+    else if (q1 === "cultural"){
+      if (q5City === "stay"){
+        $(".new-york").show();
+      }
+      else if (q5City === "abroad"){
+        (".paris").show();
+      }}
   });
 });
